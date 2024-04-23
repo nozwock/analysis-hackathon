8922344b389899950f3215871aab0b7e86cf3b4a
nozwock <57829219+nozwock@users.noreply.github.com>
2024-04-23 16:08:39 +0530

    Init commit

---
+++ analysis_hackathon/pages/Future.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.write(
+    """
+    # Future
+    Invidiae, despicationes adversantur voluptatibus, sic amicitiae non posse iucunde vivi, nisi sapienter, honeste 
+    iusteque vivatur, nec sapienter, honeste, iuste, nisi iucunde. Neque enim civitas in seditione beata esse 
+    potest nec in discordia dominorum domus; quo minus omnes mea legant. utinam esset ille Persius, Scipio vero 
+    et Rutilius multo etiam magis.
+"""
+)
