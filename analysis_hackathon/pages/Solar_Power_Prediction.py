2a5b397e328884686f602e35a56cf10c4cd842d2
nozwock <57829219+nozwock@users.noreply.github.com>
2024-04-25 16:50:50 +0530

    Update streamlit prediction pages

--- analysis_hackathon/pages/Solar_Power_Prediction.py
+++ analysis_hackathon/pages/Solar_Power_Prediction.py
@@ -1,13 +1,44 @@
+import pandas as pd
 import streamlit as st
 
-from ..models.solar_power import SolarPowerRegressor
+from analysis_hackathon.models.solar_power import SolarPowerRegressor
 
 
-@st.cache_data
+@st.cache_resource
 def get_solar_power_model() -> SolarPowerRegressor:
     model = SolarPowerRegressor()
     model.load_model()
     return model
 
 
-get_solar_power_model()
+@st.cache_data
+def get_prediction():
+    return sess.run(
+        [sess.get_outputs()[0].name],
+        {sess.get_inputs()[0].name: X_test.to_numpy()},
+    )[0].reshape(-1)
+
+
+model = get_solar_power_model()
+sess = model.load_model()
+
+st.write("""
+    # Solar Power Generation Prediction
+""")
+
+st.write("> **Dataset**")
+st.write(model.get_dataset().sample(10))
+
+_, X_test, _, Y_test = model.get_split_dataset()
+X_test.reset_index(drop=True, inplace=True)
+Y_test.reset_index(drop=True, inplace=True)
+
+st.line_chart(
+    pd.concat(
+        [
+            Y_test[:100],
+            pd.DataFrame({"Predicted AC Power": get_prediction()})[:100],
+        ],
+        axis=1,
+    )
+)
