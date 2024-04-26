83c892e14a6654e2c1c7e22cc1306222282c213e
nozwock <57829219+nozwock@users.noreply.github.com>
2024-04-26 07:42:10 +0530

    Pass forward args from __main__ to streamlit

--- analysis_hackfest/__main__.py
+++ analysis_hackfest/__main__.py
@@ -1,10 +1,13 @@
 import subprocess
+import sys
 from pathlib import Path
 
 
 def run():
     try:
-        subprocess.run(["streamlit", "run", "Home.py"], cwd=Path(__file__).parent)
+        subprocess.run(
+            ["streamlit", "run", "Home.py", *sys.argv[1:]], cwd=Path(__file__).parent
+        )
     except KeyboardInterrupt:
         ...
 
