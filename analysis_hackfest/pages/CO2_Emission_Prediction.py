2ada9e0eedc2278b64574398db6a1a61b221a98e
nozwock <57829219+nozwock@users.noreply.github.com>
2024-04-26 08:14:01 +0530

    Add CO2_Emission_Prediction page

--- analysis_hackfest/pages/CO2_Emission_Prediction.py
+++ analysis_hackfest/pages/CO2_Emission_Prediction.py
@@ -4,21 +4,15 @@ import streamlit as st
 import streamlit_pydantic as sp
 from pydantic import BaseModel
 
-from analysis_hackfest.models.solar_power import SolarPowerRegressor
+from analysis_hackfest.models.co2_emission import Co2EmissionRegressor
 
 
-class InputFeatures(BaseModel):
-    DAILY_YIELD: float = 5000
-    TOTAL_YIELD: float = 200_000
-    AMBIENT_TEMPERATURE: float = 30
-    MODULE_TEMPERATURE: float = 40
-    IRRADIATION: float = 0.5
-    DC_POWER: float = 100
+class InputFeatures(BaseModel): ...
 
 
 @st.cache_resource
-def get_model() -> SolarPowerRegressor:
-    model = SolarPowerRegressor()
+def get_model() -> Co2EmissionRegressor:
+    model = Co2EmissionRegressor()
     model.load_model()
     return model
 
@@ -38,7 +32,9 @@ def cached_samples():
         pd.concat(
             [
                 Y_test,
-                pd.DataFrame({"Predicted AC Power": get_prediction(X_test.to_numpy())}),
+                pd.DataFrame(
+                    {"Predicted CO2 Emission": get_prediction(X_test.to_numpy())}
+                ),
             ],
             axis=1,
         ).sample(100),
@@ -55,7 +51,7 @@ Y_test.reset_index(drop=True, inplace=True)
 data_sample, series_sample = cached_samples()
 
 st.write("""
-    # Solar Power Generation Prediction
+    # CO2 Emission Prediction
 """)
 
 st.write("### Dataset Sample")
@@ -63,18 +59,3 @@ st.write(data_sample)
 
 st.write("### Series Line Plot")
 st.line_chart(series_sample)
-
-
-st.write("### Estimater")
-
-
-@st.experimental_fragment
-def estimator():
-    input_features_data = sp.pydantic_input(key="form", model=InputFeatures)
-    inputs = np.fromiter(input_features_data.values(), dtype=np.float32).reshape(-1, 6)
-    st.html(
-        f"<p style='font-size: 1.25em;'>Predicted Ac Power: <code>{get_prediction(inputs)[0]}</code></p>",
-    )
-
-
-estimator()
